0ddb922b94368a00564894f7c3c0fda009697dea
Ario <dev@ariochain.info>
2019-09-03 20:53:38 +0200

    get initial peers from https://api.arionum.com/peers.txt

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -3,5 +3,25 @@
 # finalize setup
 echo "php ready";
 
+curl -s -o peers.txt -L https://api.arionum.com/peers.txt > /dev/null
+PEERS=`cat peers.txt`;
+
+CONFIG_TMP=/tmp/peers.config.php
+
+echo '$_config["initial_peer_list"] = [' > $CONFIG_TMP
+
+for ROW in ${PEERS};
+ do
+   # PEER=`echo $ROW |cut -d '-' -f 1`
+   echo "'$ROW',">>$CONFIG_TMP
+ done
+
+sed -i '$ s/,//' $CONFIG_TMP
+
+echo '];' >> $CONFIG_TMP
+
+cat $CONFIG_TMP>>/var/www/node/include/config.inc.php
+rm $CONFIG_TMP
+
 # php
 php-fpm
