ca034f016142cfbd200bee99bcb797ff9a747e8c
Ario <dev@ariochain.info>
2019-11-22 15:35:21 +0100

    docker instalation bash script for ubuntu 16+

--- install-docker-ce.sh
+++ install-docker-ce.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-apt-get remove docker docker-engine docker.io containerd runc
+apt-get remove docker docker-engine docker.io containerd runc -y
+
+apt-get update
 
 apt-get upgrade -y
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common git
+
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common git -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 
 cd
 
