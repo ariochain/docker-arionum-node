cd6399431f846679e19048f0597087edc7721ff3
Ario <dev@ariochain.info>
2019-11-28 01:23:08 +0100

    install

---
+++ install-docker-ce-debian.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get remove docker docker-engine docker.io containerd runc -y
+
+apt-get update
+
+apt-get upgrade -y
+
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common git -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
+
+cd
+
+git clone https://github.com/ariochain/docker-arionum-node
+mkdir -p ~/data/arionum-mariadb-data/
+cd docker-arionum-node/
+
+#docker-compose up --build -d
